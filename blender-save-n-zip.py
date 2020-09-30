b167cf2ad11d1aa79a06035f8e5d6b3acd0d4173
stibinator <stib@pureandapplied.com.au>
2020-09-30 18:17:08 +1000

    beta version

--- blender-save-n-zip.py
+++ blender-save-n-zip.py
@@ -1,62 +1,88 @@
 ######################################################################################################
-# An operator to save your file with an incremental suffix                                          #
-# Actualy partly uncommented - if you do not understand some parts of the code,                      #
-# please see further version or contact me.                                                          #
-# Author: Lapineige                                                                                  #
+# datestamps the last saved version of a project, and saves it into a zip file                       #
 # License: GPL v3                                                                                    #
 ######################################################################################################
 
 ############# Add-on description (used by Blender)
-
 bl_info = {
-    "name": "Save Incremental",
-    "description": 'Save your file with an incremental suffix',
-    "author": "Lapineige",
-    "version": (1, 7),
-    "blender": (2, 72, 0),
-    "location": "File > Save Incremental",
+    "name": "Save-n-zip",
+    "description": 'Save your file with incremental archived backups',
+    "author": "stib, thanks to Lapineige for the bare bones",
+    "version": (0, 1),
+    "blender": (2, 80, 0),
+    "location": "File > Save-n-zip",
     "warning": "",
     "wiki_url": "",
-    "tracker_url": "http://blenderlounge.fr/forum/viewtopic.php?f=18&t=736",
+    "tracker_url": "https://blob.pureandapplied.com.au/save-n-zip",
     "category": "System"}
 
 ##############
-import bpy, os, datetime
-from zipfile import ZipFile
+import bpy, os, zipfile, subprocess, re
+from datetime import datetime
+
+# the temp file that gets zipped
+tempFileName = "saveNzipTemp.blend"
 
 class FileSaveAndZip(bpy.types.Operator):
     bl_idname = "file.save_n_zip"
     bl_label = "Save N zip"
    
-    def execute(self, context):
-        if bpy.data.filepath:
-            dirPath,fileName = os.path.split(bpy.data.filepath.split('.blend')[0])
-            dateStamp = datetime.datetime.fromtimestamp(os.path.getmtime(bpy.data.filepath)).strftime('_%d-%m-%Y_%H%M%S.blend')
-            dateStampedFile = os.path.join(dirPath, fileName + dateStamp)
-
-            if os.path.isfile(dateStampedFile):
-                self.report({'WARNING'}, "Internal Error: trying to save over an existing file. Cancelled")
-                print('Tested Output: ', output)
-                return {'CANCELLED'}
-            os.rename(bpy.data.filepath, dateStampedFile)
-            zipFilePath = bpy.data.filepath + '.zip'
+    def execute(self, context):\
+        # full path to current project, if it has been saved
+        fileFullPath = bpy.data.filepath
+        if fileFullPath:
+            dirPath,fileName = os.path.split(fileFullPath.split('.blend')[0])
+            #slice off the numerical suffix: "foo_123.blend", "foo 12345.blend" "foo-1234.blend" or "foo12.blend" => "foo"
+            # the . at the start of the regex is to match at least one char for files named like 1.blend
+            suffixSearch = re.compile(r'.[\s_-]*\d*$', re.IGNORECASE).search(fileName)
+            suffixStart = suffixSearch.start()
+            baseName = None
+            if suffixSearch:
+                #offset the stat to account for the first matched wildcard
+                baseName = fileName[:suffixStart + 1] 
+            if not baseName:
+                baseName = fileName #this really shouldn't happen, but ugh, regex
+            dateStamp = datetime.fromtimestamp(os.path.getmtime(fileFullPath)).strftime('_%d-%m-%Y_%H%M%S.blend')
+            dateStampedFile = fileName + dateStamp
+            tempFileFullPath = os.path.join(dirPath, tempFileName)
+            #deal with old temp files
+            if os.path.isfile(tempFileFullPath):
+                os.remove(tempFileFullPath)
+            #move our current blend file to the temp file
+            os.rename(fileFullPath, tempFileFullPath)
+            zipFilePath = os.path.join(dirPath, (baseName + ".zip"))
             if os.path.isfile(zipFilePath):
-                with ZipFile(zipFilePath, 'a') as zipArchive:
-                    zipArchive.write(dateStampedFile)
-                    self.report({'INFO'}, "Zip: {0} - updated".format(zipFilePath))
+                # zip file exists, append new version
+                zipMode = "a"
             else:
-                with ZipFile(zipFilePath, 'w') as zipArchive:
-                    zipArchive.write(dateStampedFile)
-                    self.report({'INFO'}, "Zip: {0} - created".format(zipFilePath))
-            bpy.ops.wm.save_mainfile()
+                # create a new zip
+                zipMode = "w"
+            command = (
+                bpy.app.binary_path_python,
+                "-c", "import zipfile, os\nwith zipfile.ZipFile('{0}', '{1}') as zipArchive:\n  zipArchive.write('{2}', '{3}', zipfile.ZIP_LZMA)\nos.remove('{2}')".format(
+                    zipFilePath.replace("\\", "/"), 
+                    zipMode.replace("\\", "/"), 
+                    tempFileFullPath.replace("\\", "/"), 
+                    dateStampedFile.replace("\\", "/")),
+                )
+            # zips the file in a subprocess, because zipping large .blend files takes a while.
+            # with zipfile.ZipFile(zipFilePath, 'a') as zipArchive:
+                # zipArchive.write(fileFullPath, dateStampedFile, zipfile.ZIP_LZMA)
+                # report = "Zip {0} updated. ".format(zipFilePath)
+
+            proc = subprocess.Popen(command)
+            if (proc):
+                report = "Zipping old file to {0}. ".format(baseName + ".zip")
             
+            bpy.ops.wm.save_mainfile()
+            self.report({'INFO'}, "{0}Current File saved successfully".format(report))
+         
             ##self.report({'INFO'}, "File: {0} - Created at: {1}".format(output[len(bpy.path.abspath(d_sep)):], output[:len(bpy.path.abspath(d_sep))]))
         else:
+            #a modal dialog or invoking the save as window would be much better
             self.report({'WARNING'}, "Please save the file before incrementally saving")
         return {'FINISHED'}
-        ###### PENSER A TESTER AUTRES FICHIERS DU DOSSIER, VOIR SI TROU DANS NUMEROTATION==> WARNING
-
-
+ 
 def draw_into_file_menu(self,context):
     self.layout.separator()
     self.layout.operator('file.save_n_zip')
@@ -64,6 +90,7 @@ def draw_into_file_menu(self,context):
 def register():
     bpy.utils.register_class(FileSaveAndZip)
     bpy.types.TOPBAR_MT_file.append(draw_into_file_menu)
+    
 def unregister():
     bpy.utils.unregister_class(FileSaveAndZip)
     bpy.types.TOPBAR_MT_file.remove(draw_into_file_menu)
